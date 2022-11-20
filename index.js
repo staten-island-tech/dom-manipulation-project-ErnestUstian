a08b28d5a192eb64c115f3c0614a6421f082419c
ErnestUstian <113203260+ErnestUstian@users.noreply.github.com>
2022-11-19 20:41:16 -0500

    dfgh

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const  DOMSelectors  = {
-    button: document.getElementById("btn"),
-    title: document.getElementById("title"),
-    artist: document.getElementById("artist"),
-    url: document.getElementById("url"),
-    form: document.getElementById("form"),
-    display: document.getElementById("display"),
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  title: document.getElementById("title"),
+  artist: document.getElementById("artist"),
+  url: document.getElementById("url"),
+  form: document.getElementById("form"),
+  display: document.getElementById("display"),
 };
-DOMSelectors.form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    output();
+DOMSelectors.form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  output();
 });
-function output(){
-   DOMSelectors.display.insertAdjacentHTML("afterend",
+function output() {
+  DOMSelectors.display.insertAdjacentHTML(
+    "afterend",
     `<div class="display-card">
     <img class="display-img" src="${DOMSelectors.url.value}" /><img>
     <h2 class="display-artist">${DOMSelectors.artist.value}</h2>
     <h3 class="display-album">${DOMSelectors.title.value}</h3>
      <button class="remove btn">Remove Album</button>
 </div>`
-);
+  );
 }
-
-//const array = [document.querySelectorAll(${DOMSelectors.btn.value})]
-//array.forEach(element => console.log(element))
